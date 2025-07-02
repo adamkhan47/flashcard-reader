bb9bba2b10ebcf307da2393308f79b253d370a01
Adam Khan <adammkhan47@gmail.com>
2025-07-01 19:10:41 -0500

    Patched for new flashcard saving stuff

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const stored = JSON.parse(localStorage.getItem('localStorage1')); 
+let stored = {
+    term: [],
+    def: []
+};
 let number = 0;
 let shown = 0;
-let lengthOfArray = stored.term;
-lengthOfArray = lengthOfArray.length;
+let lengthOfArray = [];
+lengthOfArray = 0;
 function flashcardLoad() {
-    document.getElementById("flashinfo").style.visibility = "visible";
-    document.getElementById('flipThing').innerHTML = stored.term[0];
+    const listOfFlashCards = JSON.parse(localStorage.getItem('listOfAllCurrentFlashCards'));
+    let answer = prompt("Type in the name of the flashcard you want to load. Here's the ones you have saved: " + listOfFlashCards);
+
+    if (listOfFlashCards.includes(answer)) {
+        stored.term = JSON.parse(localStorage.getItem(answer)).term;
+        stored.def = JSON.parse(localStorage.getItem(answer)).def;
+        lengthOfArray = stored.term;
+        lengthOfArray = lengthOfArray.length;
+        document.getElementById("flashinfo").style.visibility = "visible";
+        document.getElementById('flipThing').innerHTML = stored.term[0];
+    }
 }
 function buttonFlip() {
     console.log("should flip");
